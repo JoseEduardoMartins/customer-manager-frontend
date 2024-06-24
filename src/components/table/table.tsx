380254baaade38eee2ff5fd1b738b34eabc41b31
Eduardo <m4rt1ns.jose@gmail.com>
2024-06-24 17:21:06 -0300

    improved table

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -6,37 +6,40 @@ export type TableType = {
 export const Table = ({ columns, rows }: TableType) => {
   return (
     <>
-      <table className="w-full pt-8 pb-10 px-6 md:px-10 shadow-md rounded-lg">
-        <thead>
-          <tr className="border-b border-neutral-600">
-            {columns?.map((column, index) => (
-              <th className="p-2 text-center" key={index}>
-                {column}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        {!!rows.length && (
-          <tbody>
-            {rows?.map((row, index1) => {
-              const values = Object.values(row);
+      {rows.length ? (
+        <table className="w-full pt-8 pb-10 px-6 md:px-10 shadow-md rounded-lg">
+          <thead>
+            <tr className="border-b border-neutral-600">
+              {columns?.map((column, index) => (
+                <th className="p-2 text-center" key={index}>
+                  {column}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          {!!rows.length && (
+            <tbody>
+              {rows?.map((row, index1) => {
+                const values = Object.values(row);
 
-              return (
-                <tr key={index1}>
-                  {values?.map((value, index2) => (
-                    <td className="p-2 text-center" key={index2}>
-                      {value}
-                    </td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        )}
-      </table>
-      <div className="w-full flex justify-center items-center p-4 text-white">
-        {!rows.length && <h2>Nenhum cliente foi encontrado</h2>}
-      </div>
+                return (
+                  <tr key={index1}>
+                    {values?.map((value, index2) => (
+                      <td className="p-2 text-center" key={index2}>
+                        {value}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          )}
+        </table>
+      ) : (
+        <div className="w-full flex justify-center items-center p-4 text-white">
+          <h2>Nenhum cliente foi encontrado</h2>
+        </div>
+      )}
     </>
   );
 };
